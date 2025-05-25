8b25c9f5857fc72ec78b9666b9434f1a28386ea0
frankline amakoye <anyashfrankly7@gmail.com>
2025-05-25 14:37:35 +0300

    Add 2-arguments.js to handle command-line inputs

---
+++ JS-Fundamentals/2-arguments.js
@@ -0,0 +1,9 @@
+const args = process.argv.length;
+
+if (args <=2) {
+   console.log("no argument");
+}else if (args === 3) {
+console.log("Argument found");
+} else {
+  console.log("Arguments found");
+}
